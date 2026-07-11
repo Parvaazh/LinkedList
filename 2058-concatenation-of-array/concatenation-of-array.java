3876a97c888ff435e987e30035f3781badc0804b
Mohammad Parvaazh Shareef <parvaazhshareef123@gmail.com>
2026-07-11 16:08:25 +0530

    Time: 1 ms (97.29%) | Memory: 47.4 MB (17.26%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -4,8 +4,6 @@ class Solution {
         int ans[]=new int[n*2];
         for(int i=0;i<n;i++){
             ans[i]=nums[i];
-        }
-        for(int i=0;i<n;i++){
             ans[i+n]=nums[i];
         }
         return ans;
